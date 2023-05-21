cdec1c2be0775eb74f449c2a4505f491c6ef04d8
benoitSafari <gregory.michalak.192@gmail.com>
2023-05-21 14:32:23 +0200

    Init storybook

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
     define: { global: 'globalThis' },
-    plugins: [react(), eslint()],
+    plugins: [
+        react(),
+        eslint({
+            exclude: ['/virtual:/**', 'node_modules/**'],
+        }),
+    ],
     server: {
         host: true,
         port: 8080,
